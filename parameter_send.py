5dee63ee2b3f6714e77c4e939c1a8825d6ebc1a6
Mohammed_Alshebli <98805351+Mo-Alshebli@users.noreply.github.com>
2023-10-10 21:53:17 +0300

    Update parameter_send.py

--- parameter_send.py
+++ parameter_send.py
@@ -1,9 +1,18 @@
 from pydantic import BaseModel, ValidationError, validator
 
-class MyModel(BaseModel):
-    query: str
+from pydantic import BaseModel, model_validator
+from pydantic.error_wrappers import PydanticError
 
-    @validator("query", pre=True, always=True)
-    def validate_query(cls, value):
-        # Your validation logic here
-        return value
+try:
+
+    class MyModel(BaseModel):
+        query: str
+        @validator("query", pre=True, always=True)
+        @model_validator
+        @classmethod
+        def _serialize(cls, x, y, z):
+            return cls
+
+except PydanticError as exc_info:
+    assert exc_info.code == 'model-validator-instance-method'
+
