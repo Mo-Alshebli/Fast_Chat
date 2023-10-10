a8c288ee2006e27c375d8651d70026e4ff941528
Mohammed_Alshebli <98805351+Mo-Alshebli@users.noreply.github.com>
2023-10-10 22:21:18 +0300

    Update parameter_send.py

--- parameter_send.py
+++ parameter_send.py
@@ -1,18 +1,13 @@
-from pydantic import BaseModel, PydanticUserError, root_validator,model_validator
+from pydantic import BaseModel, PydanticUserError, root_validator
 
 try:
     class MyModel(BaseModel):
         query: str
 
         @root_validator(pre=False, skip_on_failure=True)
-        @model_validator(pre=False, skip_on_failure=True)
-
         @classmethod
         def _serialize(cls, x, y, z):
             return cls
 
 except PydanticUserError as exc_info:
     assert exc_info.code == 'root-validator-pre-skip'
-
-
-
